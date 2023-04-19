218d9b4e192390ce9f47a1b310e6418068c641e6
Nicole Murff <mur20031@byui.edu>
2023-04-18 23:34:04 -0500

    Erased unnecesary comments

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -16,38 +16,33 @@ class Program
         if (gradeNum >= 90)
         {
             letterGrade = "A";
-            //Console.WriteLine("A");
         }
         else if (gradeNum >= 80)
         {
             letterGrade = "B";
-            //Console.WriteLine("B");
         }
         else if (gradeNum >= 70)
         {
             letterGrade = "C";
-            //Console.WriteLine("C");
         }
         else if (gradeNum >= 60)
         {
             letterGrade = "D";
-            //Console.WriteLine("D");
         }
         else
         {
             letterGrade = "F";
-            //Console.WriteLine("F");
         }
 
         Console.WriteLine($"Your grade is: {letterGrade}");
 
-        // if (gradeNum >= 70)
-        // {
-        //     Console.WriteLine("Congratulations! You passed!");
-        // }
-        // else 
-        // {
-        //     Console.WriteLine("Better luck next time.");
-        // }
+        if (gradeNum >= 70)
+        {
+            Console.WriteLine("Congratulations! You passed!");
+        }
+        else 
+        {
+            Console.WriteLine("Better luck next time.");
+        }
     }
 }
