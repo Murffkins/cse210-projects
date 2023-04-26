54f10a15b694cabf06d7348c56ffaf7f86338c38
Nicole Murff <mur20031@byui.edu>
2023-04-25 21:17:06 -0500

    Added comments

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -7,11 +7,14 @@ class Program
     {
         Console.WriteLine("Hello Prep4 World!");
 
+// Declare a new list
         List<int> numbers = new List<int>();
         int userNum = -1;
 
+// Get user input
         Console.WriteLine("Enter a list of numbers, type 0 when finished.");
 
+// Add user input to the list
         while (userNum != 0)
         {
             Console.Write("Enter a number: ");
@@ -24,13 +27,18 @@ class Program
             
         }
 
+// Calculate and print the sum of all of the numbers in the list
         Console.WriteLine($"The sum is: {numbers.Sum()}");
+
+// Calculate and print the average of the numbers
         double average = numbers.Average();
         Console.WriteLine($"The average is: {average}");
 
+// Find and print the largest number in the list
         int max = numbers.Max();
         Console.WriteLine($"The largest number is: {max}");
 
+// Find and print the smallest positive number in the list
         int minNum = int.MaxValue;
         for (int i = 0; i < numbers.Count; i++)
         {
@@ -41,6 +49,8 @@ class Program
         }
 
         Console.WriteLine($"The smallest positive number is: {minNum}");
+
+// Sort and print the numbers in the list
         numbers.Sort();
         Console.WriteLine($"The sorted list is:");
         foreach (int i in numbers)
