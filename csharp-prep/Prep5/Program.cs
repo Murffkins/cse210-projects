3c41a351af80b311a00a94399763ff7894f32081
Nicole Murff <mur20031@byui.edu>
2023-04-27 00:23:25 -0500

    Added assignment requirements

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -5,5 +5,46 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep5 World!");
+
+        DisplayWelcome();
+
+        string userName = PromptUserName();
+        int userNum = PromptUserNumber();
+        int squaredNum = SquareNumber(userNum);
+
+        DisplayResult(userName, squaredNum);
+    }
+
+    static void DisplayWelcome()
+    {
+        Console.WriteLine("Welcome to the program!");
+    }
+
+    static string PromptUserName()
+    {
+        Console.Write("Please enter your name: ");
+        string name = Console.ReadLine();
+
+        return name;
+    }
+
+    static int PromptUserNumber()
+    {
+        Console.Write("Please enter your favorite number: ");
+        int favNum = int.Parse(Console.ReadLine());
+
+        return favNum;
+    }
+    
+    static int SquareNumber(int number)
+    {
+        int squareNum = number *  number;
+
+        return squareNum;
+    }
+
+    static void DisplayResult(string name, int squareNum)
+    {
+        Console.WriteLine($"{name}, the square of your number is {squareNum}");
     }
 }
