6919c9a1c8463a5ef1980023bbd38e85f6a054fd
Nicole Murff <mur20031@byui.edu>
2023-05-17 21:53:10 -0500

    Created Fraction class and Main class

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -4,6 +4,30 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning03 World!");
+        // Console.WriteLine("Hello Learning03 World!");
+
+        Fraction f1 = new Fraction();
+        Console.WriteLine(f1.GetFractionString());
+        Console.WriteLine(f1.GetDecimalValue());
+        // f1.SetTop(2);
+        // f1.SetBottom(2);
+        // Console.WriteLine(f1.GetTop());
+        // Console.WriteLine(f1.GetBottom());
+
+        Fraction f2 = new Fraction(5);
+        Console.WriteLine(f2.GetFractionString());
+        Console.WriteLine(f2.GetDecimalValue());
+        // Console.WriteLine(f2.GetTop());
+        // Console.WriteLine(f2.GetBottom());
+
+        Fraction f3 = new Fraction(3, 4);
+        Console.WriteLine(f3.GetFractionString());
+        Console.WriteLine(f3.GetDecimalValue());
+        // Console.WriteLine(f3.GetTop());
+        // Console.WriteLine(f3.GetBottom());
+
+        Fraction f4 = new Fraction(1, 3);
+        Console.WriteLine(f4.GetFractionString());
+        Console.WriteLine(f4.GetDecimalValue());
     }
 }
